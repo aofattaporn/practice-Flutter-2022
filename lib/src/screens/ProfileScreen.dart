85cb307b6c13f648b9206547515c2c7f727fb98f
Attaporn Peungsook <aofattapon321@gmail.com>
2022-11-20 02:31:08 +0700

    test Covid bloc request http not complete

--- lib/src/screens/ProfileScreen.dart
+++ lib/src/screens/ProfileScreen.dart
@@ -6,29 +6,45 @@ import 'package:test_flutter/src/blocs/ProfilePage/covidBloc/covidState.dart';
 
 import '../data/model/CovidModel.dart';
 
-// import '../blocs/ProfilePage/jockBloc/jokeBloc.dart';
-// import '../blocs/ProfilePage/jockBloc/joke_state.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CovidBloc, CovidState>(builder: (context, state) {
-      if (state is CovidInitial) {
-        return _buildLoading();
-      } else if (state is CovidLoading) {
-        return _buildLoading();
-      } else if (state is CovidLoaded) {
-        return _buildCard(context, state.covidModel);
-      } else if (state is CovidError) {
-        return Container();
-      } else {
-        return Container();
-      }
-    });
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      child: BlocProvider(
+        create: (_) => CovidBloc(),
+        child: BlocListener<CovidBloc, CovidState>(
+          listener: (context, state) {
+            if (state is CovidError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message!),
+                ),
+              );
+            }
+          },
+          child: BlocBuilder<CovidBloc, CovidState>(
+            builder: (context, state) {
+              if (state is CovidInitial) {
+                return _buildLoading();
+              } else if (state is CovidLoading) {
+                return _buildLoading();
+              } else if (state is CovidLoaded) {
+                return _buildCard(context, state.covidModel);
+              } else if (state is CovidError) {
+                return Container();
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ),
+      ),
+    );
   }
-}
 
 Widget _buildCard(BuildContext context, CovidModel model) {
   return ListView.builder(
@@ -50,4 +66,4 @@ Widget _buildCard(BuildContext context, CovidModel model) {
   );
 }
 
-Widget _buildLoading() => Center(child: CircularProgressIndicator());
+Widget _buildLoading() => Center(child: CircularProgressIndicator());}
